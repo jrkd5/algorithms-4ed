4af5226855289ba38562c710b83b34da4796de2f
jrkd5 <jaroslaw.dulinski@gmail.com>
2021-04-18 15:40:25 +0200

    Basic implementation of Stack using linked list.

---
+++ 1. Fundamentals/AbstractDataTypes/src/Stack.java
@@ -0,0 +1,48 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Stack<Item> {
+
+    private int n;
+    private Node first;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public void push(Item item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+        n++;
+    }
+
+    public Item pop() {
+        Item item = first.item;
+        first = first.next;
+        n--;
+        return item;
+    }
+
+    public static void main(String[] args) {
+        Stack<String> s = new Stack<>();
+
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-"))
+                s.push(item);
+            else if (!s.isEmpty()) StdOut.print(s.pop() + " ");
+        }
+        StdOut.println("(" + s.size() + " left on stack)");
+    }
+}
