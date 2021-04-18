15021c8aed03e96b7e4e9f7bb2d748c77d07b4a6
jrkd5 <jaroslaw.dulinski@gmail.com>
2021-04-18 20:25:23 +0200

    Basic implementation of Bag using linked list.

---
+++ 1. Fundamentals/AbstractDataTypes/src/Bag.java
@@ -0,0 +1,56 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+import java.util.Iterator;
+
+public class Bag<Item> implements Iterable<Item>{
+
+    private Node first;
+
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    public void add(Item item) {
+        Node oldFirst = first;
+        first = new Node();
+        first.item = item;
+        first.next = oldFirst;
+    }
+
+    @Override
+    public Iterator<Item> iterator() {
+        return new ListIterator();
+    }
+
+    public class ListIterator implements Iterator<Item> {
+
+        private Node current = first;
+
+        @Override
+        public boolean hasNext() {
+            return current != null;
+        }
+
+        @Override
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    public static void main(String[] args) {
+        Bag<String> b = new Bag<>();
+
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            b.add(item);
+        }
+        StdOut.println("Items in the bag: ");
+        for (String item : b) {
+            StdOut.print(item + ", ");
+        }
+    }
+}
