93edf3588ba1950de480624e6d6c60b0bf4dbe1a
jrkd5 <jaroslaw.dulinski@gmail.com>
2021-07-03 08:02:16 +0200

    Implemented QuickFind & test client

---
+++ Coursera/Part I/UnionFind/src/TestUF.java
@@ -0,0 +1,18 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class TestUF {
+
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        UF uf = new QuickFindUF(N);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (!uf.connected(p, q)) {
+                uf.union(p, q);
+                StdOut.println(p + " " + q);
+            }
+        }
+    }
+}
---
+++ Coursera/Part I/UnionFind/src/UF.java
@@ -0,0 +1,5 @@
+public interface UF {
+
+    void union(int p, int q);
+    boolean connected(int p, int q);
+}
